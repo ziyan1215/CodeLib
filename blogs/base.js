052914f16dbc60e4f5f8bf39b2944536d936b2d7
ziyan1215 <xuziyanmiss@gmail.com>
2016-07-24 18:37:25 +0800

    begin a demo about blogs

--- blogs/base.js
+++ blogs/base.js
@@ -20,7 +20,7 @@ var base = {
 }
 */
 
-//对象式
+/*//对象式
 var Base = {
 	getId : function(id){
 		return document.getElementById(id);
@@ -31,4 +31,117 @@ var Base = {
 	getTagName : function(tagName){
 		return document.getElementsByTagName(tagName);
 	},
+}*/
+
+var $ = function(){
+	return new Base;
 }
+
+//基础库
+function Base(){
+	//创建一个数组，用来保存获取的节点和节点数组
+	this.elements =[];
+	//获取ID
+	this.getId = function(id){
+		this.elements.push(document.getElementById(id));
+		return this;
+	}
+
+	//获取TagName
+	this.getTagName = function(tagName){
+		var tags = document.getElementsByTagName(tagName);
+		for(var i = 0 ;i<tags.length;i++){
+			this.elements.push(tags[i]);
+		}
+		return this;
+	}
+
+
+}
+
+/*//设置CSS
+Base.prototype.css = function(attr,value){
+	for(var i = 0 ; i<this.elements.length;i++){
+		if(arguments.length == 1){ //只有一个参数的时候  获取该CSS属性值
+			return this.elements[i].style[attr];
+		}
+			this.elements[i].style[attr]=value;
+	
+	}
+	return this;
+}*/
+
+//设置CSS
+Base.prototype.css = function(attr,value){
+	for(var i = 0 ; i<this.elements.length;i++){
+		if(arguments.length == 1){ //只有一个参数的时候  获取该CSS属性值
+			//兼容获取外部样式  .getComputedStyle()获取的是最终应用在元素上的所有CSS属性对象
+			if(typeof window.getComputedStyle != 'undefined'){ //w3c
+				return window.getComputedStyle(this.elements[i],null)[attr];
+			}else if(typeof this.elements[i] != 'undefined'){ //ie
+				return this.elements.style[attr];
+			}
+		}
+			this.elements[i].style[attr]=value;
+	
+	}
+	return this;
+}
+
+//设置innerHTML
+Base.prototype.html = function(str){
+
+	for(var i =0;i<this.elements.length;i++){
+		if(arguments.length==0){//假如没有参数的时候  就获取该element的innerHTML值
+		return this.elements[i].innerHTML;
+	}
+		this.elements[i].innerHTML = str;
+	}
+	return this;
+}
+
+//触发点击事件
+Base.prototype.click=function(fn){
+	for(var i =0;i<this.elements.length;i++){
+		this.elements[i].onclick = fn;
+	}
+}
+
+/*//获取class节点数组
+Base.prototype.getClass=function(className){
+	var all = document.getElementsByTagName("*");
+	for(var i=0;i<all.length;i++){
+		if(all[i].className==className){
+			this.elements.push(all[i]);
+		}
+	}
+	return this;
+}*/
+//获取class节点数组，增加一个获取区间的参数
+Base.prototype.getClass=function(className,idName){
+	var node = null;
+	if(arguments.length==2){
+		node = document.getElementById(idName);
+	}else{
+		node=document;
+	}
+	var all = node.getElementsByTagName("*");
+	for(var i=0;i<all.length;i++){
+		if(all[i].className==className){
+			this.elements.push(all[i]);
+		}
+	}
+	return this;
+}
+
+
+
+//获取某一个节点
+Base.prototype.getElement = function(num){
+	var element = this.elements[num]; //新建一个变量存储获取的class对象
+	this.elements = []; //清空获取的对象
+	this.elements[0] = element;//给返回值赋值为获取的第几个对象
+	return this;
+
+}
+
--- blogs/demo.js
+++ blogs/demo.js
@@ -3,9 +3,29 @@
 	alert(document.getElementsByName("sex")[0].value);
 	alert(document.getElementsByTagName("p")[0].innerHTML);
 }*/
-window.onload = function(){
+/*window.onload = function(){
 
 	alert(Base.getId("box").innerHTML);
 	alert(Base.getName("sex")[0].value);
 	alert(Base.getTagName("p")[0].innerHTML);
 }
+Base 是一个基础库核心对象，返回的是divElement 这个对象没有CSS方法*/
+
+window.onload = function(){
+	//var base =new Base ;
+	/*$().getId("box").css("color","red").css("background","blue");
+	alert($().getTagName("p").elements.length);
+	$().getTagName("p").css("color","red").css("background","green");
+	$().getTagName("p").html("段落").click(function(){
+		alert("a");*/
+	// var i= $().getTagName("p");//返回元素P的对象集合,document.getElementsByTagName("p")返回的是带有指定标签名的对象集合.length
+	//	alert(i.elements[1].innerHTML);
+	//	
+		//alert($().getId("box").css("color"));
+		//alert($().getId("box").css("color"));
+	//$().getClass('p','box1').css('color','red');
+	$().getId("box1").css("color","red");
+	//$().getClass('p').css("color","green");
+	//$().getClass('p').css("color",'yellow');
+
+	};
